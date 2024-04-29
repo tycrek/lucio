f29d27f967fbfda138acddb1acd1a589ad193298
Sylvie <sylvie@tycrek.com>
2024-04-28 18:59:18 -0600

    feat: added random voice lines

--- src/twilio.ts
+++ src/twilio.ts
@@ -1,7 +1,11 @@
+import * as fs from 'fs-extra';
+import { path } from '@tycrek/joint';
 import twilio, { twiml } from 'twilio';
 import VoiceResponse from 'twilio/lib/twiml/VoiceResponse';
 
 const smsClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+const lines = fs.readFileSync(path.join('voice-lines.txt')).toString().split('\n');
 
 const wait = (r: VoiceResponse) => r.pause({ length: 1 });
 const tone = (r: VoiceResponse) => {
@@ -24,5 +28,7 @@ export const alert = () =>
 	smsClient.messages.create({
 		from: process.env.SMS_FROM,
 		to: process.env.SMS_TO,
-		body: 'Door open'
+		body: lines[rand(0, lines.length - 1)]
 	});
+
+const rand = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min);
